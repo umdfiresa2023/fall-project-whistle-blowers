b9ac23b9b2f049076c623412878b9704b9a0769d
tjones77umd <tjones77@terpmail.umd.edu>
2023-10-23 19:49:23 +0000

    test

--- ESG Index - PPG.R
+++ ESG Index - PPG.R
@@ -3,41 +3,31 @@ install.packages("lubridate")
 install.packages("lfe")
 install.packages("tidytext")
 install.packages("SnowballC")
+install.packages("textdata")
 
-
+library(textdata)
 library(tidyverse)
 library(dplyr)
 library(tidytext)
 library(SnowballC)
 
 # reading the text file 
-test_data <- read.table("2011.txt", sep = ".", header = TRUE, row.names = NULL)
+test_data <- readLines("2011.txt")
 
-# #tokenization and removing stop words
-# test_data_tokenize <- test_data %>%
-#   unnest_tokens(output = "word",
-#                 token = "words",
-#                 input = X10.K) %>%
-#   anti_join(stop_words) %>%
-#   count(word, sort = TRUE)
-# 
-# 
-# #Stemming 
-# stemmed_test_data <- test_data_tokenize %>%
-#   mutate(word = wordStem(word))%>%
-#   count(word, sort = TRUE)
 
-#sentence tokenization
-test_data_sentences <- test_data %>%
+
+df <- tibble(text = test_data)
+
+test_data_sentences <- df %>%
   unnest_tokens(output = "sentence",
                 token = "sentences",
-                input = X10.K) 
+                input = text) 
 
 #the total score of emotions
 total_score <- 0
 
 #for loop because words used separately as environment/environmental/environmentally
-for(term in c("environment", "environmental", "environmentally")) {
+for(term in c("profits")) {
  
   #considering the environment related sentences
   env_sentences <- test_data_sentences[grepl(term, test_data_sentences$sentence), ]
@@ -47,10 +37,12 @@ for(term in c("environment", "environmental", "environmentally")) {
     unnest_tokens(output = "word", token = "words", input = sentence) %>%
     anti_join(stop_words)
   
+  afinnframe<-get_sentiments("afinn")
   # Use afinn to find the overall sentiment score
   affin_score <- env_tokens %>% 
-    inner_join(get_sentiments("afinn")) %>%
-    summarise(sentiment = sum(score))
+    inner_join(afinnframe, by = c("word" = "word")) %>%
+    summarise(sentiment = sum(value))
   
   total_score = total_score + affin_score
 }
+
